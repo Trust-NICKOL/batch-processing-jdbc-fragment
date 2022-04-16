c8611175c65ecaa7aa441e4fbe473972f5925e2b
Alexander Nickol <a.nickol@trust-nickol.de>
2022-04-16 11:50:00 +0200

    JUnit-Tests for the batch insert fragment

---
+++ src/test/java/de/anickol/batch_insert_jdbc_fragment/TestApplication.java
@@ -0,0 +1,12 @@
+package de.anickol.batch_insert_jdbc_fragment;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Simple SpringBoot Application.
+ *
+ * @author anickol
+ */
+@SpringBootApplication
+public class TestApplication {
+}
